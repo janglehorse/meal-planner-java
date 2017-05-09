c3ae1dc21f6c9586c8f2531471e16202d9a9d404
J. Caleb Travers <jcalebtravers@icloud.com>
2017-05-09 13:56:38 -0500

    Basic add recipe view

--- src/main/java/com/calebtravers/controllers/RecipeController.java
+++ src/main/java/com/calebtravers/controllers/RecipeController.java
@@ -1,10 +1,16 @@
 package com.calebtravers.controllers;
 
+import com.calebtravers.models.Recipe;
 import com.calebtravers.models.data.RecipeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by adminbackup on 5/3/17.
@@ -27,7 +33,31 @@ public class RecipeController {
 
     }
 
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String DisplayAddForm(Model model){
 
+        model.addAttribute("title", "Add Recipe");
+        model.addAttribute(new Recipe());
+        return "recipe/add";
+
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String ProcessAddForm (@ModelAttribute @Valid Recipe newRecipe,
+                                  Errors errors,
+                                  Model model){
+
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add Recipe");
+            return "recipe/add";
+        }
+        else{
+            recipeDao.save(newRecipe);
+            return "redirect: ";
+
+        }
+
+    }
 
 
 }
