6ce7d5d7c59fc46d4793253b09c40da26a6a0e17
J. Caleb Travers <jcalebtravers@icloud.com>
2017-05-26 11:30:16 -0500

    Add User, Role model

---
+++ src/main/java/com/calebtravers/models/Role.java
@@ -0,0 +1,48 @@
+package com.calebtravers.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+/**
+ * Created by adminbackup on 5/26/17.
+ */
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    private Set<User> users;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToMany(mappedBy = "roles")
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/com/calebtravers/models/User.java
@@ -0,0 +1,74 @@
+package com.calebtravers.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by adminbackup on 5/26/17.
+ */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=24)
+    private String username;
+
+    @NotNull
+    @Size(min=3, max=24)
+    private String password;
+
+    @NotNull
+    @Size(min=3, max=24)
+    private String passwordConfirm;
+
+    private Set<Role> roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Transient
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    @ManyToMany
+    @JoinTable(name="user_role", joinColumns = @JoinColumn(name="user_id"),
+                inverseJoinColumns = @JoinColumn(name="role_id"))
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
