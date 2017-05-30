4fd5202e1c51d32d9fa4778800a6ccc013892202
J. Caleb Travers <jcalebtravers@icloud.com>
2017-05-30 14:27:29 -0500

    Update MealplannerApplication.java

--- src/main/java/com/calebtravers/MealplannerApplication.java
+++ src/main/java/com/calebtravers/MealplannerApplication.java
@@ -2,11 +2,18 @@ package com.calebtravers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MealplannerApplication {
+public class MealplannerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {SpringApplication.run(MealplannerApplication.class, args);
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(MealplannerApplication.class);
+    }
+
+	public static void main(String[] args) throws Exception {SpringApplication.run(MealplannerApplication.class, args);
 	}
 
 }
